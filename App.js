8551bf865b62af43df3f1b0992b9f43a821eb4cf
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2021-05-09 19:41:04 -0500

    Code for profile screen

--- App.js
+++ App.js
@@ -2,19 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-import LogInFields from "./components/logInFields";
-import LogInHeader from "./components/logInHeader";
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <LogInHeader />
-      <View style={styles.elements}>
-        <Image 
-          source={ require("./images/Untitled.png")}
-          style={{ width: 349, height: 190, marginBottom: 20 }}
-        />
-        <LogInFields />
+      <View style={styles.body}>
+        <View>
+          <Text style={styles.gameTitle}>Apex Legends</Text>
+        </View>
+        <View style={styles.profileBanner}> 
+          <Image 
+            style = {styles.profilePic}
+            source = {require("./images/pfp-placeholder.jpg")}
+          />
+          <Text style={styles.username}>Username</Text>
+        </View>
+        <View style = {styles.playerStats}>
+          <Text style = {styles.statsTitle}>Level:</Text>
+          <Text style = {styles.statsData}>0</Text>
+          <Text style = {styles.statsTitle}>Rank:</Text>
+          <Text style = {styles.statsData}>Bronze 4</Text>
+        </View>
       </View>
     </View>
   );
@@ -22,11 +29,46 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#F5F6F8',
+    backgroundColor: '#2e2a5d',
   },
-  elements: {
-    justifyContent: 'center',
-    padding: 40
+  body: {
+    marginTop: 75,
+    marginLeft: 25,
+    marginBottom: 25
+  },
+  profilePic: {
+    width: 150,
+    height: 150,
+    borderWidth: 3,
+    borderColor: "black",
+  },
+  profileBanner: {
+    flexDirection: 'row',
+  },
+  username: {
+    marginLeft: 20,
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#fff"
+  },
+  playerStats: {
+    marginTop: 20,
+  },
+  statsTitle:{
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#fff"
+  },
+  statsData: {
+    fontSize: 18,
+    color: "#fff"
+  },
+  gameTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#fff",
+    borderBottomWidth: 2,
+    borderColor: "#068592"
   }
 });
